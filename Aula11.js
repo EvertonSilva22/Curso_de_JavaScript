7ad8517745bd5616cda23aba53b463a3bb21d3cf
perkles <alvesevertonx@gmail.com>
2022-09-16 21:35:41 -0300

    Aula_11
    Aula sobre condições, (if e else).

--- Aula11.js
+++ Aula11.js
@@ -1,2 +1,13 @@
 console.log('O console funcionou corretamente!')
+
+var vel = 50
+
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}else{
+
+console.log(`Dirija sempre com cuidado e utilizando o cinto de segurança!`)
+}
 //SIS.PARVIS.MAGNA// 
