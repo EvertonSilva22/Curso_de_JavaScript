546fdf32b6a0f538581343ea9e15e676830833cd
perkles <alvesevertonx@gmail.com>
2022-09-30 18:45:58 -0300

    Exercício Horas
    Adicionando a mensagem de Bom Dia, Boa Tarde ou Boa Noite.

--- Aula_13/JavaScript/script.js
+++ Aula_13/JavaScript/script.js
@@ -3,19 +3,22 @@ function carregar(){
     var imagem = window.document.getElementById('imagem')
     var data = new Date()
     //var hora = data.getHours()
-    var hora = 8
+    var hora = 18
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >=0 && hora <12){
         //Bom Dia!
         imagem.src = 'imagens/fotomanha.jpg'
         document.body.style.background = '#fbe697'
+        msg.innerHTML += `<p>Bom Dia!</p>`
     } else if (hora >=12 && hora <18){
         //Boa Tarde!
         imagem.src = 'imagens/fototarde.jpg'
         document.body.style.background = '#fc5d01'
+        msg.innerHTML += `<p>Boa Tarde!</p>`
     }else {
         //Boa Noite!
         imagem.src = 'imagens/fotonoite.jpg'
         document.body.style.background = '#151723'
+        msg.innerHTML += `<p>Boa Noite!</p>`
     }
 }
