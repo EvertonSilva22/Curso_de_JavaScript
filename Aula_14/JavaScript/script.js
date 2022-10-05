04a5f27a005288441a12c8235982deefb738a3b5
perkles <alvesevertonx@gmail.com>
2022-10-04 21:20:08 -0300

    .
    .

--- Aula_14/JavaScript/script.js
+++ Aula_14/JavaScript/script.js
@@ -16,31 +16,41 @@ function Verificar() {
             if (idade >=0 && idade < 2){
                 //Bebe
                 img.setAttribute('src', './imagens/bebe-menino.jpg')
+
             } else if (idade < 10){
                 //Criança
+                img.setAttribute('src', './imagens/menino.jpg')
             } else if (idade < 18){
                 //Jovem
+                img.setAttribute('src', './imagens/Homem-jovem.jpg')
             } else if(idade < 50){
                 //Adulto
+                img.setAttribute('src', './imagens/Homem-Adulto.jpg')
             } else{
                 // Idoso
+                img.setAttribute('src', './imagens/Homem-Idoso.jpg')
             }
         } else if (fsex[1].checked){
             genero = 'Mulher'
             if (idade >=0 && idade <2){
                 //Bebe
+                img.setAttribute('src', './imagens/Bebe-Menina.jpg')
             } else if (idade < 10){
                 //Criança
+                img.setAttribute('src', './imagens/Menina.jpg')
             } else if (idade < 18){
                 //Jovem
+                img.setAttribute('src', './imagens/Menina-Jovem.jpg')
             } else if(idade < 50){
                 //Adulto
+                img.setAttribute('src', './imagens/Mulher-Adulta.jpg')
             } else{
                 // Idoso
+                img.setAttribute('src', './imagens/Mulher-Idosa.jpg')
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
         res.appendChild(img)
     }
 }
