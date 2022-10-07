06f8bdd0edbe02c49aba3643e8491958fb103114
perkles <alvesevertonx@gmail.com>
2022-10-07 16:44:12 -0300

    Aula_15
    Aprendendo sobre estrutura de repetição em JavaScript. utilização do "WHILE" e do "DO"

---
+++ Aula_15/script.js
@@ -0,0 +1,10 @@
+var pizza = 1
+do {
+    console.log(`Comer pedaço de pizza ${pizza}`)
+    pizza++
+}while (pizza <= 8)
+
+//while (contar <= 8){
+    //console.log(`contar ${contar}`) estrutura de repetição utilizando apenas o While. 
+    //contar++
+//}
